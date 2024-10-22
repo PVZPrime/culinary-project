fdfd189ef4fd042be18c97000991562f2fbc9e85
Devin <mcmanusdevin2008@gmail.com>
2024-10-22 13:28:45 -0700

    added test script

---
+++ culinary-project/Assets/Scripts/TestScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject[] amounts = GameObject.FindGameObjectsWithTag("amounts");
+        GameObject[] costs = GameObject.FindGameObjectsWithTag("costs");
+        amounts[0, 7, 23, 0, ];
+        costs[0.2, 0.1, 0.3, ];
+    }
+}
