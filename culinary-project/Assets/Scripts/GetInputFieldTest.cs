963dd4c644d832db9c346bf42f9599ccb56ff91a
PVZPrime <Pvshaw32@gmail.com>
2024-10-25 12:53:37 -0700

    fixed the math that was not working
    The math was all in one line, this would cause the whole math code to not work. The reason it would not work is because it was trying to divide by zero if any of the input boxes had a zero.

--- culinary-project/Assets/Scripts/GetInputFieldTest.cs
+++ culinary-project/Assets/Scripts/GetInputFieldTest.cs
@@ -126,115 +126,180 @@ public class GetInputFieldTest : MonoBehaviour
     }
     void Calculate()
     {
+        float flourAmt = -1;
         float flourCost = -1;
+        float flourProduct = -1;
         Debug.Log(i_flourAmt.text);
         Debug.Log(float.TryParse(i_flourCost.text, out flourCost));
-        float flourAmt = -1;
         Debug.Log(float.TryParse(i_flourAmt.text, out flourAmt));
-        float flourProduct = -1;
         Debug.Log(float.TryParse(i_flourProduct.text, out flourProduct));
-
+        float flourTotalCost = 0;
+        if (flourCost > 0 && flourAmt > 0 && flourProduct > 0)
+        {
+            flourTotalCost = flourAmt * flourCost / flourProduct;
+        }
+        
+        float saltAmt = -1;
         float saltCost = -1;
+        float saltProduct = -1;
         Debug.Log(i_saltAmt.text);
         Debug.Log(float.TryParse(i_saltCost.text, out saltCost));
-        float saltAmt = -1;
         Debug.Log(float.TryParse(i_saltAmt.text, out saltAmt));
-        float saltProduct = -1;
         Debug.Log(float.TryParse(i_saltProduct.text, out saltProduct));
+        float saltTotalCost = 0;
+        if (saltCost > 0 && saltAmt > 0 && saltProduct > 0)
+        {
+            saltTotalCost = saltAmt * saltCost / saltProduct;
+        }
 
+        float OilAmt = -1;
         float OilCost = -1;
+        float OilProduct = -1;
         Debug.Log(i_OilAmt.text);
         Debug.Log(float.TryParse(i_OilCost.text, out OilCost));
-        float OilAmt = -1;
         Debug.Log(float.TryParse(i_OilAmt.text, out OilAmt));
-        float oilProduct = -1;
-        Debug.Log(float.TryParse(i_oilProduct.text, out oilProduct));
+        Debug.Log(float.TryParse(i_oilProduct.text, out OilProduct));
+        float OilTotalCost = 0;
+        if (OilCost > 0 && OilAmt > 0 && OilProduct > 0)
+        {
+            OilTotalCost = OilAmt * OilCost / OilProduct;
+        }
 
         float sugarCost = -1;
+        float sugarAmt = -1;
+        float sugarProduct = -1;
         Debug.Log(i_sugarAmt.text);
         Debug.Log(float.TryParse(i_sugarCost.text, out sugarCost));
-        float sugarAmt = -1;
         Debug.Log(float.TryParse(i_sugarAmt.text, out sugarAmt));
-        float sugarProduct = -1;
         Debug.Log(float.TryParse(i_sugarProduct.text, out sugarProduct));
+        float sugarTotalCost = 0;
+        if (sugarCost > 0 && sugarAmt > 0 && sugarProduct > 0)
+        {
+            sugarTotalCost = sugarAmt * sugarCost / sugarProduct;
+        }
 
+        float MilkAmt = -1;
         float MilkCost = -1;
+        float MilkProduct = -1;
         Debug.Log(i_MilkAmt.text);
         Debug.Log(float.TryParse(i_MilkCost.text, out MilkCost));
-        float MilkAmt = -1;
         Debug.Log(float.TryParse(i_MilkAmt.text, out MilkAmt));
-        float MilkProduct = -1;
         Debug.Log(float.TryParse(i_MilkProduct.text, out MilkProduct));
+        float MilkTotalCost = 0;
+        if (MilkCost > 0 && MilkAmt > 0 && MilkProduct > 0)
+        {
+            MilkTotalCost = MilkAmt * MilkCost / MilkProduct;
+        }
 
+        float heavyCreamAmt = -1;
         float heavyCreamCost = -1;
+        float heavyCreamProduct = -1;
         Debug.Log(i_heavyCreamAmt.text);
         Debug.Log(float.TryParse(i_heavyCreamCost.text, out heavyCreamCost));
-        float heavyCreamAmt = -1;
         Debug.Log(float.TryParse(i_heavyCreamAmt.text, out heavyCreamAmt));
-        float heavyCreamProduct = -1;
         Debug.Log(float.TryParse(i_heavyCreamProduct.text, out heavyCreamProduct));
+        float heavyCreamTotalCost = 0;
+        if (heavyCreamCost > 0 && heavyCreamAmt > 0 && heavyCreamProduct > 0)
+        {
+            heavyCreamTotalCost = heavyCreamAmt * heavyCreamCost / heavyCreamProduct;
+        }
 
+        float eggsAmt = -1;
         float eggsCost = -1;
+        float eggsProduct = -1;
         Debug.Log(i_eggsAmt.text);
         Debug.Log(float.TryParse(i_eggsCost.text, out eggsCost));
-        float eggsAmt = -1;
         Debug.Log(float.TryParse(i_eggsAmt.text, out eggsAmt));
-        float eggsProduct = -1;
         Debug.Log(float.TryParse(i_eggsProduct.text, out eggsProduct));
+        float eggsTotalCost = 0;
+        if (eggsCost > 0 && eggsAmt > 0 && eggsProduct > 0)
+        {
+            eggsTotalCost = eggsAmt * eggsCost / eggsProduct;
+        }
 
+        float yeastAmt = -1;
         float yeastCost = -1;
+        float yeastProduct = -1;
         Debug.Log(i_yeastAmt.text);
         Debug.Log(float.TryParse(i_yeastCost.text, out yeastCost));
-        float yeastAmt = -1;
         Debug.Log(float.TryParse(i_yeastAmt.text, out yeastAmt));
-        float yeastProduct = -1;
         Debug.Log(float.TryParse(i_yeastProduct.text, out yeastProduct));
+        float yeastTotalCost = 0;
+        if (yeastCost > 0 && yeastAmt > 0 && yeastProduct > 0)
+        {
+            yeastTotalCost = yeastAmt * yeastCost / yeastProduct;
+        }
 
+        float BakingPowderAmt = -1;
         float BakingPowderCost = -1;
+        float BakingPowderProduct = -1;
         Debug.Log(i_BakingPowderAmt.text);
         Debug.Log(float.TryParse(i_BakingPowderCost.text, out BakingPowderCost));
-        float BakingPowderAmt = -1;
         Debug.Log(float.TryParse(i_BakingPowderAmt.text, out BakingPowderAmt));
-        float BakingPowderProduct = -1;
         Debug.Log(float.TryParse(i_BakingPowderProduct.text, out BakingPowderProduct));
+        float BakingPowderTotalCost = 0;
+        if (BakingPowderCost > 0 && BakingPowderAmt > 0 && BakingPowderProduct > 0)
+        {
+            BakingPowderTotalCost = BakingPowderAmt * BakingPowderCost / BakingPowderProduct;
+        }
 
+        float butterAmt = -1;
         float butterCost = -1;
+        float butterProduct = -1;
         Debug.Log(i_butterAmt.text);
         Debug.Log(float.TryParse(i_butterCost.text, out butterCost));
-        float butterAmt = -1;
         Debug.Log(float.TryParse(i_butterAmt.text, out butterAmt));
-        float butterProduct = -1;
         Debug.Log(float.TryParse(i_butterProduct.text, out butterProduct));
+        float butterTotalCost = 0;
+        if (butterCost > 0 && butterAmt > 0 && butterProduct > 0)
+        {
+            butterTotalCost = butterAmt * butterCost / butterProduct;
+        }
 
+        float BakingSodaAmt = -1;
         float BakingSodaCost = -1;
+        float BakingSodaProduct = -1;
         Debug.Log(i_BakingSodaAmt.text);
         Debug.Log(float.TryParse(i_BakingSodaCost.text, out BakingSodaCost));
-        float BakingSodaAmt = -1;
         Debug.Log(float.TryParse(i_BakingSodaAmt.text, out BakingSodaAmt));
-        float BakingSodaProduct = -1;
         Debug.Log(float.TryParse(i_BakingSodaProduct.text, out BakingSodaProduct));
+        float BakingSodaTotalCost = 0;
+        if (BakingSodaCost > 0 && BakingSodaAmt > 0 && BakingSodaProduct > 0)
+        {
+            BakingSodaTotalCost = BakingSodaAmt * BakingSodaCost / BakingSodaProduct;
+        }
 
+        float potatoesAmt = -1;
         float potatoesCost = -1;
+        float potatoesProduct = -1;
         Debug.Log(i_potatoesAmt.text);
         Debug.Log(float.TryParse(i_potatoesCost.text, out potatoesCost));
-        float potatoesAmt = -1;
         Debug.Log(float.TryParse(i_potatoesAmt.text, out potatoesAmt));
-        float potatoesProduct = -1;
         Debug.Log(float.TryParse(i_potatoesProduct.text, out potatoesProduct));
+        float potatoesTotalCost = 0;
+        if (potatoesCost > 0 && potatoesAmt > 0 && potatoesProduct > 0)
+        {
+            potatoesTotalCost = potatoesAmt * potatoesCost / potatoesProduct;
+        }
 
+        float carrotsAmt = -1;
         float carrotsCost = -1;
+        float carrotsProduct = -1;
         Debug.Log(i_carrotsAmt.text);
         Debug.Log(float.TryParse(i_carrotsCost.text, out carrotsCost));
-        float carrotsAmt = -1;
         Debug.Log(float.TryParse(i_carrotsAmt.text, out carrotsAmt));
-        float carrotsProduct = -1;
         Debug.Log(float.TryParse(i_carrotsProduct.text, out carrotsProduct));
+        float carrotsTotalCost = 0;
+        if (carrotsCost > 0 && carrotsAmt > 0 && carrotsProduct > 0)
+        {
+            carrotsTotalCost = carrotsAmt * carrotsCost / carrotsProduct;
+        }
 
         float MarkUp = -1;
         Debug.Log(float.TryParse(i_MarkUp.text, out MarkUp));
 
         Debug.Log(yeastAmt * yeastCost + eggsAmt * eggsCost + heavyCreamAmt * heavyCreamCost * sugarAmt * sugarCost + OilAmt * OilCost + saltAmt * saltCost + flourAmt * flourCost);
-        MyText.text = "$" + ((carrotsAmt * (carrotsCost / carrotsProduct) + potatoesAmt * (potatoesCost / potatoesProduct) + BakingSodaAmt * (BakingSodaCost / BakingSodaProduct) + butterAmt * (butterCost / butterProduct) + BakingPowderAmt * (BakingPowderCost / BakingPowderProduct)+ MilkAmt * (MilkCost / MilkProduct) + yeastAmt * (yeastCost / yeastProduct) + eggsAmt * (eggsCost / eggsProduct)+ heavyCreamAmt * (heavyCreamCost / heavyCreamProduct) + sugarAmt * (sugarCost / sugarProduct)+ OilAmt * (OilCost / oilProduct)+ saltAmt * (saltCost / saltProduct)+ flourAmt * (flourCost / flourProduct)*MarkUp));
+        MyText.text = "$" + ((MarkUp)*(yeastTotalCost + saltTotalCost + heavyCreamTotalCost + sugarTotalCost + BakingPowderTotalCost + BakingSodaTotalCost + MilkTotalCost + potatoesTotalCost + carrotsTotalCost + OilTotalCost + butterTotalCost + eggsTotalCost));
 
 
     }
